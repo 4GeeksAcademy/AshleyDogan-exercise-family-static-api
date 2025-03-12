2f4f1f7f178ff52c503b72e62a47b0bcaa444e90
Ashley Dogan <a.a.rappette@gmail.com>
2025-03-12 03:31:32 +0000

    5 passed

--- src/app.py
+++ src/app.py
@@ -12,8 +12,34 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
+
 jackson_family = FamilyStructure("Jackson")
+
+jackson_family.add_member(
+    {
+        'first_name': 'John',
+        'age': 33,
+        'lucky_numbers': [7, 13, 22]    
+    }
+)
+
+jackson_family.add_member(
+    {
+        'first_name': 'Jane',
+	    'age': 35,
+	    'lucky_numbers': [10, 14, 3]
+    
+    }
+)
+
+jackson_family.add_member(
+    {
+        'first_name': 'Jimmy',
+		'age': 5,
+        'lucky_numbers': [1]
+    }
+)
+
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -26,17 +52,38 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
+def get_all_members():
     members = jackson_family.get_all_members()
     response_body = {
-        "hello": "world",
         "family": members
     }
+    return jsonify(response_body['family']), 200
 
-
+@app.route('/members', methods=['POST'])
+def add_new_member():
+    new_family_member = request.json
+    jackson_family.add_member(new_family_member)
+    members = jackson_family.get_all_members()
+    response_body = {
+        "results: members"
+    }
     return jsonify(response_body), 200
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_single_member(id):
+    member = jackson_family.get_member(id)
+    return jsonify(member), 200
+
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_single_member(id):
+    member = jackson_family.get_member(id)
+ 
+    if member:
+        jackson_family.delete_member(id)
+        return jsonify({"message": "Member deleted"}), 200
+    else:
+        return jsonify({"error": "Member not found"}), 404
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
